962a242b31ecf80de640d8c5605485e4c83635a7
netocavallini <netocavallini@gmail.com>
2020-11-15 06:02:42 +0000

    (26/10) Início do primeiro componente de listagem

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CursoListComponent } from './curso/curso-list/curso-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // Nomes de rota no Angular (path) NÃO começam com barra, diferente do back-end
+    {path: 'curso', component: CursoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ front-end/src/app/curso/curso-list/curso-list.component.ts
@@ -0,0 +1,26 @@
+import { CursoService } from './../curso.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-list',
+  templateUrl: './curso-list.component.html',
+  styleUrls: ['./curso-list.component.scss']
+})
+export class CursoListComponent implements OnInit {
+
+  // Nome da entidade no plural
+  cursos : any = [] 
+
+  //Quais Colunas serão exibidas na tabela e em qual ordem
+  displayedColumns: String[] = ['nome', 'carga_horaria', 'nivel', 'valor_curso']
+  
+  // Injeção de dependência ou inversão de controle
+  constructor(private cursoSrv : CursoService) { }
+
+  // Ambiente ideal para armazenar dados
+  async ngOnInit() {
+    this.cursos = await this.cursoSrv.listar()
+    console.log(this.cursos)
+  }
+
+}
